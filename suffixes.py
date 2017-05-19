3b7f91b089156cd6329fd682b1e1908b4dd604ce
Tanny <nerdySingh@users.noreply.github.com>
2017-05-18 23:10:08 -0500

    generates suffixes.py

--- suffixes.py
+++ suffixes.py
@@ -5,16 +5,16 @@ mybool=[]
 def check(f,suff):
     flag = True
     for i in range(len(f)):
-        end_len = len(f[i])
-        start_len = end_len- len(suff)
-        word = f[i][start_len:end_len]
-        my_match=re.findall(suff,word)
+        end_len = len(f[i]) #checks for the length of the string.
+        start_len = end_len- len(suff) #checks for the length of the suffix to be found in the string.
+        word = f[i][start_len:end_len] #the concatenation of the start and the end length.
+        my_match=re.findall(suff,word) #regex to use to to find any matches in the string
         if len(my_match)>0:
             flag = True
             wordlist.append(f[i])
         else:
             flag=False
-    return True
+    return len(wordlist)
 
 
 if __name__ == "__main__":
@@ -25,6 +25,11 @@ if __name__ == "__main__":
    
     suff = input("Enter the suffix string:")
     flag=check(f,suff)
-    if flag == True:
-        print(wordlist)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    if flag > 0:
+        #print(wordlist)
+        thefile = open('suffixes.txt', 'w')#opens a file named suffixes.txt to be written.
+        for item in wordlist:
+            thefile.write("%s\n" % item) #writes the word into every new line of the file.
+        print("Check the suffixes.txt file for the output values to be generated. ") #the code is printed to be done.
+    else:
+        print("No such suffixes were found.")
